48c2a3a87a85f4294dfec46666ebf7d3acfc70b2
devinhyeok <94621970+devinhyeok@users.noreply.github.com>
2022-11-28 00:54:47 +0900

    fix_router_bug

--- components/NavBar.js
+++ components/NavBar.js
@@ -11,7 +11,7 @@ export default function NavBar() {
                     <a className={router.pathname === "/" ? "active" : ""}>Home</a>
                 </Link>
                 <Link legacyBehavior href="/about">
-                    <a className={router.pathname === "/about" ? "active" : ""}>About</a>
+                    <a className={router.pathname === `${process.env.PUBLIC_URL}/about` ? "active" : ""}>About</a>
                 </Link>
             </div>
             <style jsx>{`
