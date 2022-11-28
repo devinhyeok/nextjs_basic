2789b0451294cf7060b77201c0910df25ffae7f3
devinhyeok <94621970+devinhyeok@users.noreply.github.com>
2022-11-28 15:30:18 +0900

    npm-run-export-error-fix

--- pages/movies/[...params].js
+++ pages/movies/[...params].js
@@ -1,13 +1,11 @@
 
 import { useRouter } from 'next/router';
-import { Seo } from '../../components/Seo';
 
-export default function Detail({ params }) {
+export default function Detail() {
     const router = useRouter();
-    const [title, id] = params || [];
+    const [title, id] = router.query.params;
     return (
         <div>
-            <Seo title={title} />
             <h4>{title}</h4>
         </div>
     );
