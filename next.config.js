020529f4e49c82ebb6d61962a6b7a1a465388c85
devinhyeok <94621970+devinhyeok@users.noreply.github.com>
2022-11-28 13:12:15 +0900

    ch2_4_homepage_done

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
 /** @type {import('next').NextConfig} */
 const debug = process.env.NODE_ENV !== "production";
 const repository = "nextjs_basic";
+const API_KEY = process.env.API_KEY;
 
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+  async redirects() {
+    return [{
+      source: "/old-blog/:path*",
+      destination: "/new-sexy-blog/:path*",
+      permanent: false,
+    }];
+  },
+  async rewrites() {
+    return [{
+      source: "/api/movies",
+      destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
+    }];
+  }
 }
 
 module.exports = nextConfig
